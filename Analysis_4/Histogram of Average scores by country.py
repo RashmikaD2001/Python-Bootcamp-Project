401080f410d56f373277640a945505ae6097123a
isuru797 <as2021543@sci.sjp.ac.lk>
2024-12-10 16:43:07 +0530

    Update data set -- Histogram of Average scores by country

--- Analysis_4/Histogram of Average scores by country.py
+++ Analysis_4/Histogram of Average scores by country.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-url = "https://raw.githubusercontent.com/Dhanushkaprabhath2001/DashBoardDemo/refs/heads/main/data/cricket_data%20(2).csv"
+url = "https://raw.githubusercontent.com/RashmikaD2001/Python-Bootcamp-Project/refs/heads/main/cleaned_data.csv"
 crick_df = pd.read_csv(url)
 
-import pandas as pd
+
 
 # Group by 'team_1' and calculate total runs and average
 team_stats = crick_df.groupby('team_1')['team_1_runs'].agg(['sum', 'mean'])
@@ -14,11 +15,6 @@ team_stats = team_stats.rename(columns={'sum': 'Total Runs', 'mean': 'Average'})
 
 # Display the table
 print(team_stats)
-
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Assuming 'crick_df' is your DataFrame
 
 # --- Average runs for team_1 ---
 team_1_avg_runs = crick_df.groupby('team_1')['team_1_runs'].mean().sort_values(ascending=False)
@@ -32,7 +28,7 @@ plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
 
-import pandas as pd
+
 
 # Group by 'team_2' and calculate total runs and average
 team_stats = crick_df.groupby('team_2')['team_2_runs'].agg(['sum', 'mean'])
@@ -46,7 +42,7 @@ print(team_stats)
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Assuming 'crick_df' is your DataFrame
+
 
 # --- Average runs for team_2 ---
 team_2_avg_runs = crick_df.groupby('team_2')['team_2_runs'].mean().sort_values(ascending=False)
